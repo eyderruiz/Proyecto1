b192dfab7f37b03fc7001ed5209f7abe36ac4b87
Eyder Ruiz <eyder_ruiz82151@elpoli.edu.co>
2021-03-05 20:19:40 -0500

    Primer commit, creando el proyecto

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html LANG = ES>
+<head>
+    <title>Proyecto heroku</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="css/normalize.css"/>
+    <link rel="stylesheet" href="css/estilos.css"/>
+</head>
+<body>
+    <div id="envoltura">
+        <header>
+            <h1>Programaci&oacute;n Web</h1>
+        </header>
+        <div id="main">
+            <p>
+                En este curso aprenderaz a programar paginas web usando git.
+                <?php 
+                    $curso = "contruccion de elementos de software 2";
+                echo "<br/><br/> estas en el curso: $curso" ?>
+            </p>
+            <p>
+                En este curso aprenderaz a programar paginas web usando git.
+                <?php 
+                    $curso = "contruccion de elementos de software 2";
+                echo "<br/><br/> estas en el curso: $curso" ?>
+            </p>
+        </div>
+            
+    </div>
+</body>
+</html>
