e1ed0bbd4c0756daadee297f203572666094fa9e
Eyder Ruiz <eyder_ruiz82151@elpoli.edu.co>
2021-03-05 20:46:21 -0500

    segundo commit

---
+++ pagina2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html LANG = ES>
+<head>
+    <title>Proyecto heroku</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="css/normalize.css"/>
+    <link rel="stylesheet" href="css/estilos.css"/>
+</head>
+<body>
+    
+    <?php 
+        $a = 12;
+        $b = 1000;
+
+        $a .= $b;
+
+        echo "<br/>concatenado  $a." 
+
+    ?>
+
+
+
+</body>
+</html>
